efa78f392f687f3012fa7b8fbd818cb939ad3c8f
clickcracker <play.clickcracker@gmail.com>
2024-10-25 19:25:16 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 
+# เข้าไปในโฟลเดอร์ที่ Clone มา
+cd sms
+
 # อัปเดตและอัปเกรดแพ็กเกจ
 pkg update && pkg upgrade -y
 
-# ติดตั้ง Python และแพ็กเกจที่จำเป็น
-pkg install python -y
-pkg install git -y
 pkg install termux-api -y
-
-# Clone repository
-git clone https://github.com/clickcracker/sms
-
-# เข้าไปในโฟลเดอร์ที่ Clone มา
-cd sms
 
 # ติดตั้ง OpenSSL และไลบรารี Python
 pkg install openssl -y
